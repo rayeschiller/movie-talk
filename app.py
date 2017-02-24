af7ac04c84d5478e947e2199122ee2a1fadd324a
Your Name <you@example.com>
2017-02-24 12:51:43 -0500

    test

--- app.py
+++ app.py
@@ -104,5 +104,6 @@ if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
     print("Starting app on port %d" % port)
+    print("rina")
 
     app.run(debug=False, port=port, host='0.0.0.0')
