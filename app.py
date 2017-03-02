e1c97f1ff9fb5a66e009ff0a8a8a63b0e1a4b45e
Your Name <you@example.com>
2017-03-02 13:03:14 -0500

    tried to change link to search query

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ import json
 import os
 import datetime
 import logging
+import inspect
+
+local_vars ={}
 
 from flask import Flask
 from flask import request
@@ -37,43 +40,45 @@ def webhook():
 
 
 def processRequest(req):
+    global local_vars
     if req.get("result").get("action") != "movieReleaseDate":
         return {}
-    baseurl = "https://api.themoviedb.org/3/search/movie?api_key=9fe2fdf8fcbeeb11ecec17e5e4f0276a&query="
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + yql_query
-    print(yql_url)
-    result = urlopen(yql_url).read()
+  
+    baseurl = "https://api.themoviedb.org/3/search/movie?api_key=9fe2fdf8fcbeeb11ecec17e5e4f0276a&query=Jack+Reacher"
+#    yql_query = makeYqlQuery(req)
+#    if yql_query is None:
+#        return {}
+#    yql_url = baseurl + yql_query
+#   print(yql_url)
+    print(baseurl)   
+    result = urlopen(baseurl).read()
     data = json.loads(result)
     res = makeWebhookResult(data)
+    local_vars= inspect.currentframe().f_locals
     return res
 
 
 def makeYqlQuery(req):
     result = req.get("result")
     parameters = result.get("parameters")
-#    title = parameters.get("title")
-    title = "anastasia"
+    title = parameters.get("title")
+#    title = "anastasia"
     if title is None:
         return None
-
     return title
 
 
 def makeWebhookResult(data):
-    query = data.get('results')
-    if query is None:
+    results = data.get('results')
+    if results is None:
         return {}
 
-    date = query.get('release_date')
+    date = results.get('release_date')
     date = datetime.datetime.strptime(date, '%Y-%m-%d').strftime('%m/%d/%Y')
-    
     if date is None:
         return {}
         
-    title = data.get('title')
+    title = results.get('title')
     if title is None:
         return {}
 
@@ -110,6 +115,7 @@ def makeWebhookResult(data):
 
 
 if __name__ == '__main__':
+   
     port = int(os.getenv('PORT', 5000))
 
     print("Starting app on port %d" % port)
